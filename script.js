a06f9e5cfcd643a03c5029bac3b428560adb54e8
wenkexing2007 <wenkexing.07@gmail.com>
2025-05-13 01:49:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function moveRandoE1(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+}
+
+const moveRandom = document.querySelector("#move-random")
+
+moveRandom.addEventListener("mouseenter", function  (e) {
+    moveRandoE1(e.target);
+});
